177775654cff0ddf8c61afe027cec51c7fc3f4ed
Aki <Stephanie.fermil@gmail.com>
2026-02-25 11:06:59 +0800

    Added app UI

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1024,
-    height: 768,
+    width: 950,
+    height: 900,
+    resizable: false,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true
